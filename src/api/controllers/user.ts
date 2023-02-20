95dd577a0d88ffd6f0839aa0bc31c9c8f4143ccf
Juan Hernandez <hernandezjuanm39@gmail.com>
2023-02-19 23:49:43 -0600

    added user update and retrieve controller functions

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -37,12 +37,64 @@ export const listUsers: RequestHandler = async (req, res, next) => {
 
 export const deleteUser: RequestHandler = async (req, res, next) => {
   try {
-    const deletedUserCount = await User.destroy({
+    const deletedUserCount: number = await User.destroy({
       where: { id: req.params.id },
     });
+
+    if (deletedUserCount == 0) {
+      return res.status(404).json({ message: "User could not be found" });
+    }
 
     return res.status(200).json({ message: "User has been deleted" });
   } catch (error) {
     return res.status(500).json({ message: "User could not be deleted" });
+  }
+};
+
+// TODO: Need to only get the data that we need to update
+export const updateUser: RequestHandler = async (req, res, next) => {
+  const userInfo = {
+    id: req.body.id,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    phone: req.body.phone,
+    email: req.body.email,
+    department: req.body.department,
+  };
+
+  try {
+    const arr = await User.update(
+      {
+        FirstName: userInfo.firstname,
+        LastName: userInfo.lastname,
+        Phone: userInfo.phone,
+        Email: userInfo.email,
+        Department: userInfo.department,
+      },
+      {
+        where: { id: userInfo.id },
+      }
+    );
+
+    return res.status(200).json({
+      message: "User information was updated",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Unable to update user due to" });
+  }
+};
+
+export const retrieveUser: RequestHandler = async (req, res, next) => {
+  try {
+    const userInfo = await User.findByPk(req.params.id);
+
+    if (userInfo == null) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    return res.status(200).json({ message: "User found!", data: userInfo });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Unable to retrieve user!" });
   }
 };
--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -1,10 +1,19 @@
-import { createUser, deleteUser, listUsers } from "../controllers/user";
+import {
+  createUser,
+  deleteUser,
+  listUsers,
+  retrieveUser,
+  updateUser,
+} from "../controllers/user";
 import { Router } from "express";
 
 const userRouter = Router();
 
 userRouter.get("/", listUsers);
 userRouter.post("/", createUser);
+
 userRouter.delete("/:id", deleteUser);
+userRouter.put("/:id", updateUser);
+userRouter.get("/:id", retrieveUser);
 
 export default userRouter;
